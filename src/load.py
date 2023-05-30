c8072a4503057af216760d461b7480072d1657c5
Thomas Fraunholz <tf@wogra.com>
2023-05-30 13:12:28 +0200

    reduce to single profile

--- src/load.py
+++ src/load.py
@@ -1,15 +1,17 @@
 import boto3
 from datasets import load_dataset
 
+def download_data(
+           endpoint: str,
+           bucket: str,
+           path: str,
+           credential_path: str = '.dvc/.minio_credentials'):
+    os.environ['AWS_SHARED_CREDENTIALS_FILE'] = credential_path
 
-def download_data(endpoint_url, bucket_name, file_name):
     s3 = boto3.resource("s3", endpoint_url=endpoint_url)
     bucket = s3.Bucket(bucket_name)
-    bucket.download_file(file_name, "data.zip")
-    get_ipython().system("rm -r data")
-    get_ipython().system("unzip data.zip -d data")
+    bucket.download_file(path, "data.zip")
     return "data"
-
 
 endpoint_url = "https://storage.s3.mlops.wogra.com"
 bucket_name = "data"
