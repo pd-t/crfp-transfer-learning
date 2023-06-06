bac9b718392dd078c8f70fcef4da7b1a8df5cdc3
Thomas Fraunholz <tf@wogra.com>
2023-06-06 12:47:24 -0700

    revert to old dvc repo

--- auto-dvc-commit.sh
+++ auto-dvc-commit.sh
@@ -1,43 +1,28 @@
 #!/bin/bash
 
-echo '[DvC] execute dvc pipeline'
+echo '[DVC] pull dvc data'
 dvc pull || true
-dvc repro
-dvc_repro_exit_status=$?
-echo "[DvC] dvc repro exit status: $dvc_repro_exit_status"
 
-if [ $dvc_repro_exit_status -eq 0 ]; then
-    echo "[DvC] dvc repro executed successfully."
-else
-    echo "[DvC] dvc repro failed."
+echo '[DVC] dvc repro'
+if dvc repro
+  then
+    echo '[DVC] dvc repro success!'
+  else
+    echo '[DVC] dvc repro error!'
+    exit 1
 fi
 
-dvc commit -q
-dvc push -v
+dvc commit
+dvc push
 git add dvc.lock
 
-if [ $dvc_repro_exit_status -eq 0 ]; then
-    git commit -m "[DvC] dvc.lock changes."
-else
-    git commit -m "[DvC] dvc.lock changes. [skip ci]"
-fi
-git_commit_dvc_lock=$?
-echo "[DvC] git commit exit status: $git_commit_dvc_lock"
-
-if [ $git_commit_dvc_lock -eq 0 ]; then
-    echo "[DvC] dvc.lock changed. Commit dvc.lock."
-    export GIT_SSL_NO_VERIFY=1
-    git push https://CICD_TOKEN:"$CICD_TOKEN"@"$CI_SERVER_HOST"/"$CI_PROJECT_PATH".git HEAD:"$CI_COMMIT_REF_NAME"
-    if [ $dvc_repro_exit_status -eq 0 ]; then
-        exit 0
-    else
-        exit 1
-    fi 
-else
-    echo "[DvC] dvc.lock not changed. Nothing to commit."
-    if [ $dvc_repro_exit_status -eq 0 ]; then
-        exit 0
-    else
-        exit 1
-    fi
+echo '[DVC] git commit'
+if git commit -m '[DVC] Add new dvc.lock'
+  then
+    echo '[DVC] dvc.lock changed. Pipeline restarts to prevent release. This is not an error!'
+    export GIT_SSL_NO_VERIFY=1  
+    git push https://DVC_TOKEN:"$DVC_TOKEN"@"$CI_SERVER_HOST"/"$CI_PROJECT_PATH".git HEAD:"$CI_COMMIT_REF_NAME"
+    exit 1
+  else
+    echo '[DVC] dvc is up-to-date, ready for release!'
 fi
