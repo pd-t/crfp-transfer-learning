d2e085b8a4f676edc6678d93780f470f7bca1eb3
Thomas Fraunholz <tf@wogra.com>
2023-06-06 09:16:01 -0700

    another try

--- auto-dvc-commit.sh
+++ auto-dvc-commit.sh
@@ -15,17 +15,18 @@ fi
 dvc commit
 dvc push
 git add dvc.lock
-git_add_dvc_lock=$?
-echo "[DvC] git add dvc.lock exit status: $git_add_dvc_lock"
 
-if [ $git_add_dvc_lock -eq 0 ]; then
-    export GIT_SSL_NO_VERIFY=1
+if [ $dvc_repro_exit_status -eq 0 ]; then
+    git commit -m "[DvC] dvc.lock changes."
+else
+    git commit -m "[DvC] dvc.lock changes. [skip ci]"
+fi
+git_commit_dvc_lock=$?
+echo "[DvC] git commit exit status: $git_commit_dvc_lock"
+
+if [ $git_commit_dvc_lock -eq 0 ]; then
     echo "[DvC] dvc.lock changed. Commit dvc.lock."
-    if [ $dvc_repro_exit_status -eq 0 ]; then
-        git commit -m "[DvC] dvc.lock changes."
-    else
-        git commit -m "[DvC] dvc.lock changes. [skip ci]"
-    fi 
+    export GIT_SSL_NO_VERIFY=1
     git push https://CICD_TOKEN:"$CICD_TOKEN"@"$CI_SERVER_HOST"/"$CI_PROJECT_PATH".git HEAD:"$CI_COMMIT_REF_NAME"
     if [ $dvc_repro_exit_status -eq 0 ]; then
         exit 0
