f2028b4a85f7c9554bda46e3ef83b02106fab38f
Thomas Fraunholz <tf@wogra.com>
2023-06-06 09:03:35 -0700

    fix minor bug

--- auto-dvc-commit.sh
+++ auto-dvc-commit.sh
@@ -4,6 +4,7 @@ echo '[DvC] execute dvc pipeline'
 dvc pull || true
 dvc repro
 dvc_repro_exit_status=$?
+
 if [ $dvc_repro_exit_status -eq 0 ]; then
     echo "[DvC] dvc repro executed successfully."
 else
@@ -16,16 +17,19 @@ git add dvc.lock
 git_add_dvc_lock=$?
 
 if [ $git_add_dvc_lock -eq 0 ]; then
+    export GIT_SSL_NO_VERIFY=1
     echo "[DvC] dvc.lock changed. Commit Changes."
     if [ $dvc_repro_exit_status -eq 0 ]; then
         git commit -m "[DvC] dvc.lock changes."
-        exit 0
     else
         git commit -m "[DvC] dvc.lock changes. [skip ci]"
+    fi 
+    git push https://CICD_TOKEN:"$CICD_TOKEN"@"$CI_SERVER_HOST"/"$CI_PROJECT_PATH".git HEAD:"$CI_COMMIT_REF_NAME"
+    if [ $dvc_repro_exit_status -eq 0 ]; then
+        exit 0
+    else
         exit 1
     fi 
-    export GIT_SSL_NO_VERIFY=1
-    git push https://CICD_TOKEN:"$CICD_TOKEN"@"$CI_SERVER_HOST"/"$CI_PROJECT_PATH".git HEAD:"$CI_COMMIT_REF_NAME"
 else
     echo "[DvC] dvc.lock not changed. Nothing to commit."
     if [ $dvc_repro_exit_status -eq 0 ]; then
