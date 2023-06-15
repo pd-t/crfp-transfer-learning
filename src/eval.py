4960a028a46aa9e4390ea6a062b6fb0dbadcc105
Thomas Fraunholz <tf@wogra.com>
2023-06-15 11:55:46 +0200

    bla

--- src/eval.py
+++ src/eval.py
@@ -22,8 +22,7 @@ def write_metrics(trainer_log, keys, file_name):
     write_json(file_name, metrics)
 
 def evaluate(trainer_log):
-    write_values_to_json(trainer_log, "eval_loss", "loss.json")
-    write_values_to_json(trainer_log, "eval_accuracy", "accuracy.json")
+    write_values_to_json(trainer_log, "eval_loss", "plots.json")
     write_metrics(trainer_log, ["eval_loss", "eval_accuracy"], "metrics.json")
 
 if __name__=='__main__':
