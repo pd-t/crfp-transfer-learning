71a3b19cb344fdbb96da6175b75dfd4f6b8a12bb
Thomas Fraunholz <tf@sourcemaker.org>
2024-02-01 22:36:59 +0100

    remove things and add README.md

---
+++ README.md
@@ -0,0 +1,11 @@
+devcontainer.env
+
+``` bash
+AWS_ACCESS_KEY_ID = <YOUR_AWS_ACCESS_KEY_ID>
+AWS_SECRET_ACCESS_KEY = <AWS_SECRET_ACCESS_KEY>
+```
+
+
+``` bash
+dvc repro
+```
--- create_minio_credentials.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CREDENTIAL_FILE="./.s3_credentials"
-
-echo "[default]" > $CREDENTIAL_FILE
-echo "aws_access_key_id=$S3_AWS_ACCESS_KEY_ID" >> $CREDENTIAL_FILE
-echo "aws_secret_access_key=$S3_AWS_SECRET_ACCESS_KEY" >> $CREDENTIAL_FILE
-
